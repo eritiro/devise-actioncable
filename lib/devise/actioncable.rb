62cdbd43da8ec9069042ad9f8dc022deee25840c
Emiliano Ritiro <eritiro@gmail.com>
2015-08-25 18:56:42 -0300

    working version

---
+++ lib/devise/actioncable.rb
@@ -0,0 +1,29 @@
+require "devise/actioncable/version"
+
+module Devise
+  module Actioncable
+    def self.glue_devise
+class_eval <<-METHODS, __FILE__, __LINE__ + 1
+    def self.helper_method *ignore
+    end
+
+    def warden
+      @request.env['warden'] ||= begin
+        manager = Warden::Manager.new(nil) do |config|
+          config.merge! Devise.warden_config
+        end
+        Warden::Proxy.new(@request.env, manager)
+      end
+    end
+
+    include Devise::Controllers::Helpers
+
+
+    # I'm so hardcore
+    def self.identified_by(*identifiers)
+      self.identifiers += identifiers
+    end
+METHODS
+    end
+  end
+end
---
+++ lib/devise/actioncable/version.rb
@@ -0,0 +1,5 @@
+module Devise
+  module Actioncable
+    VERSION = "0.1.0"
+  end
+end
